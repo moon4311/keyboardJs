de3793153e9019a2956e7fad89f78e1c470fa202
kjm <kjm@GRAM_KJM>
2020-11-09 18:49:59 +0900

    혼합받침 다음글자 변환 처리

--- WebContent/js/keyboard.js
+++ WebContent/js/keyboard.js
@@ -71,35 +71,65 @@ $(document).ready(function(){
 			var t2 = (cVal - 44032 -t3)/28 % 21; //중성
 			var t1 = (((cVal-44032 -t3)/28)-t2) / 21; //초성
 			
+			//복함모음
+			if(t3==0 && t2>0 && middleKeyList.indexOf(char)>-1){
+				console.log("복합모음");
+				switch (t2) {
+				case 8: //ㅗ
+					if(char=='ㅏ') cVal += 1*28;
+					if(char=='ㅐ') cVal += 2*28;
+					if(char=='ㅣ') cVal += 3*28;
+					break;
+				case 13: //ㅜ
+					if(char=='ㅓ') cVal += 1*28;
+					if(char=='ㅔ') cVal += 2*28;
+					if(char=='ㅣ') cVal += 3*28;
+					break;
+				case 18: //ㅡ
+					if(char=='ㅣ') cVal += 1*28;
+					break;
+				default:
+					break;
+				}
+				fnModHanGul(String.fromCharCode(cVal));
+				return;
 			//받침 넣기
-			if(t3==0 && endKeyList.indexOf(char)>-1){
+			}else if(t3==0 && endKeyList.indexOf(char)>-1){
+				console.log("받침넣기");
 				t3 = endKeyList.indexOf(char);
 				cVal += t3;
 				fnModHanGul(String.fromCharCode(cVal));
+				return;
 			//복합받침
-			}else if(t3==1||t3==4||t3==8||t3==17){
-				
+			}else if(t3==1||t3==4||t3==8||t3==17){ //ㄱ,ㄴ,ㄹ,ㅂ
+				console.log("복합받침");
 				switch (t3) {
 				case 1: //ㄱ
-					if(char=='ㅅ') cVal += 1;
+					if(char=='ㅅ') cVal += 2;
+					else 
+						return trgt.val(trgt.val()+char);
 					break;
 				case 4://ㄴ
 					if(char=='ㅈ') cVal += 1;
-					if(char=='ㅎ') cVal += 2;
+					else if(char=='ㅎ') cVal += 2;
+					else return trgt.val(trgt.val()+char);
 					break;
 				case 8: //ㄹ
 					if(char=='ㄱ') cVal += 1;
-					if(char=='ㅁ') cVal += 2;
-					if(char=='ㅂ') cVal += 3;
-					if(char=='ㅅ') cVal += 4;
-					if(char=='ㅌ') cVal += 5;
-					if(char=='ㅍ') cVal += 6;
-					if(char=='ㅎ') cVal += 7;
+					else if(char=='ㅁ') cVal += 2;
+					else if(char=='ㅂ') cVal += 3;
+					else if(char=='ㅅ') cVal += 4;
+					else if(char=='ㅌ') cVal += 5;
+					else if(char=='ㅍ') cVal += 6;
+					else if(char=='ㅎ') cVal += 7;
+					else return trgt.val(trgt.val()+char);
 					break;
-				case 17:
+				case 17: //ㅂ
 					if(char=='ㅅ') cVal += 1;
+					else return trgt.val(trgt.val()+char);
 					break;
 				default:
+					return trgt.val(trgt.val()+char);
 					break;
 				}
 				fnModHanGul(String.fromCharCode(cVal));
@@ -108,9 +138,45 @@ $(document).ready(function(){
 				trgt.val(trgt.val()+char);
 			//받침 뺴서 자음으로 넣기
 			}else if(middleKeyList.indexOf(char)>-1){
-				cVal -= t3;
-				var rslt = String.fromCharCode(cVal);
+				var minus = t3;
 				var startKey = endKeyList[t3];
+				
+				switch (t3){
+				case 3: //ㄳ
+					minus=2;	startKey = "ㅅ";
+					break;
+				case 5: //ㄵ
+					minus=1;	startKey = "ㅈ";
+					break;
+				case 6:	//ㄶ
+					minus=2;	startKey = "ㅎ";
+					break;
+				case 9: //ㄺ
+					minus=1;	startKey = "ㄱ";
+					break;
+				case 10: //ㄻ
+					minus=2;	startKey = "ㅁ";
+					break;
+				case 11: //ㄼ
+					minus=3;	startKey = "ㅂ";
+					break;
+				case 12: //ㄽ
+					minus=4;	startKey = "ㅅ";
+					break;
+				case 13: //ㄾ
+					minus=5;	startKey = "ㅌ";
+					break;
+				case 14: //ㄿ
+					minus=6;	startKey = "ㅍ";
+					break;
+				case 15: //ㅀ
+					minus=7;	startKey = "ㅎ";
+					break;
+				default: 
+					break;
+				}
+				cVal -= minus;
+				var rslt = String.fromCharCode(cVal);
 				rslt += toHanGul(startKeyList.indexOf(startKey),middleKeyList.indexOf(char));
 				fnModHanGul(rslt);
 			}
